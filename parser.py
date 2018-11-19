d771f45d569690274e6b02bfda180375c69d4d0d
felagos <felagossanchez@gmail.com>
2018-11-18 23:07:35 -0300

    agrego parse para resumen de los resultados de las instancias

---
+++ parser.py
@@ -0,0 +1,21 @@
+files = ["AB-TCVCS", "CC-TCVCS", "CR-AA10CS", "HW-TCVCS", "PT-TCVCS", "PV-TCVCS", "RC-AA10CS", "RD-AA10CS", "TC-TCVCS", "VC-TCVCS", "WR-TCVCS"]
+
+for instancia in files:
+	path = "./Resultados CS/"+instancia
+	fileNew = open("./Resultados CS/Resumen CS/"+instancia+".txt", "w")
+
+	for i in range(1, 16):
+		fileName = path+str(i)+".txt"
+		file = open(fileName, "r")
+
+		lines = file.readlines()
+		line = lines[-1].split(",")
+		
+		try:
+			fileNew.write(line[15]+"\n")
+		except:
+			print(line, fileName)
+
+		file.close()
+
+	fileNew.close()
