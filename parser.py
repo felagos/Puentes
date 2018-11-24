33e6e581e5f897c9e69165d73201978a92a9b9fa
felagos <felagossanchez@gmail.com>
2018-11-23 23:18:18 -0300

    Resultados parciales de DA

--- parser.py
+++ parser.py
@@ -1,21 +1,24 @@
 files = ["AB-TCVCS", "CC-TCVCS", "CR-AA10CS", "HW-TCVCS", "PT-TCVCS", "PV-TCVCS", "RC-AA10CS", "RD-AA10CS", "TC-TCVCS", "VC-TCVCS", "WR-TCVCS"]
 
 for instancia in files:
-	path = "./Resultados CS/"+instancia
-	fileNew = open("./Resultados CS/Resumen CS/"+instancia+".txt", "w")
+	path = "./Resultados DA/"+instancia
+	try:
+		fileNew = open("./Resultados DA/Resumen DA/"+instancia+".txt", "w")
 
-	for i in range(1, 16):
-		fileName = path+str(i)+".txt"
-		file = open(fileName, "r")
+		for i in range(1, 16):
+			fileName = path+str(i)+".txt"
+			file = open(fileName, "r")
 
-		lines = file.readlines()
-		line = lines[-1].split(",")
-		
-		try:
-			fileNew.write(line[15]+"\n")
-		except:
-			print(line, fileName)
+			lines = file.readlines()
+			line = lines[-1].split(",")
+			
+			try:
+				fileNew.write(line[15]+"\n")
+			except:
+				print(line, fileName)
 
-		file.close()
+			file.close()
 
-	fileNew.close()
+		fileNew.close()
+	except Exception as e:
+		print(e)
